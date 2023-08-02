6ce47835819e838810396431a821104482dec498
Apsara <budhtahokiapsara5@gmail.com>
2023-08-02 21:32:33 +0545

    update

--- react_optimization/src/App.js
+++ react_optimization/src/App.js
@@ -1,21 +1,32 @@
-import React,{useState} from 'react';
+import React, { useState, useCallback } from 'react';
 import Button from './component/UI/Button';
 import DemoOutput from './component/Demo/DemoOutput';
 import './App.css';
 
 function App() {
-const [showParagraph, setShowParagraph] =useState(false);
-console.log('App Running');
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setallowToggle] = useState(false);
 
-const toggleParagraphHandler = () => {
-  setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-};
+
+  console.log('App Running');
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+
+  const allowToggleHandler = () => {
+    setallowToggle(true);
+  }
 
   return (
     <div className="top_part">
-     <h1>Hi there!</h1>
-    <DemoOutput show={false} />
-     <Button onClick={toggleParagraphHandler}>Click me</Button>
+      <h1>Hi there!</h1>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggle!</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
 
   );
